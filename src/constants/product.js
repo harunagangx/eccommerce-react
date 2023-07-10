798f6ac519d10d221e2ac2049387db74b84231f4
magnolia <mtruongggg@gmail.com>
2023-07-11 02:07:12 +0700

    init

---
+++ src/constants/product.js
@@ -0,0 +1,261 @@
+import bed_dalat from '../images/bed-dalat.jpg';
+import bed_doclap from '../images/bed-doclap.jpg';
+import bed_lullaby from '../images/bed-lullaby.jpg';
+import bed_quada from '../images/bed-quada.jpg';
+import bed_sago from '../images/bed-sago.jpg';
+
+import chair_dumbo from '../images/chair-dumbo.jpg';
+import chair_minov from '../images/chair-minov.jpg';
+import chair_moana from '../images/chair-moana.jpg';
+import chair_oliver from '../images/chair-oliver.jpg';
+import chair_poppy from '../images/chair-poppy.jpg';
+
+import frame_inside from '../images/frame-inside.jpg';
+import frame_min from '../images/frame-min.jpg';
+import frame_sunset from '../images/frame-sunset.jpg';
+
+import lamp_anco from '../images/lamp-anco.jpg';
+import lamp_midu from '../images/lamp-midu.jpg';
+import lamp_ythus from '../images/lamp-ythus.jpg';
+
+import sofa_aladdin from '../images/sofa-aladdin.jpg';
+import sofa_anastasia from '../images/sofa-anastasia.jpg';
+import sofa_flynn from '../images/sofa-flynn.jpg';
+import sofa_jasmin from '../images/sofa-aladdin.jpg';
+import sofa_michau from '../images/sofa-michau.jpg';
+
+const products = [
+  {
+    id: '01',
+    name: 'dalat',
+    imgUrl: bed_dalat,
+    price: 415,
+    category: 'bed',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '02',
+    name: 'doclap',
+    imgUrl: bed_doclap,
+    price: 415,
+    category: 'bed',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '03',
+    name: 'lullaby',
+    imgUrl: bed_lullaby,
+    price: 444,
+    category: 'bed',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '04',
+    name: 'quada',
+    imgUrl: bed_quada,
+    price: 456,
+    category: 'bed',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '05',
+    name: 'sago',
+    imgUrl: bed_sago,
+    price: 374,
+    category: 'bed',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '06',
+    name: 'dumbo',
+    imgUrl: chair_dumbo,
+    price: 274,
+    category: 'chair',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '07',
+    name: 'minov',
+    imgUrl: chair_minov,
+    price: 162,
+    category: 'chair',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '08',
+    name: 'moana',
+    imgUrl: chair_moana,
+    price: 336,
+    category: 'chair',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '09',
+    name: 'oliver',
+    imgUrl: chair_oliver,
+    price: 397,
+    category: 'chair',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '10',
+    name: 'poppy',
+    imgUrl: chair_poppy,
+    price: 82,
+    category: 'chair',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '11',
+    name: 'inside',
+    imgUrl: frame_inside,
+    price: 36,
+    category: 'frame',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '12',
+    name: 'min',
+    imgUrl: frame_min,
+    price: 25,
+    category: 'frame',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '13',
+    name: 'sunset',
+    imgUrl: frame_sunset,
+    price: 59,
+    category: 'frame',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '14',
+    name: 'anco',
+    imgUrl: lamp_anco,
+    price: 95,
+    category: 'lamp',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '15',
+    name: 'midu',
+    imgUrl: lamp_midu,
+    price: 84,
+    category: 'lamp',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '16',
+    name: 'ythus',
+    imgUrl: lamp_ythus,
+    price: 87,
+    category: 'lamp',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '17',
+    name: 'aladdin',
+    imgUrl: sofa_aladdin,
+    price: 655,
+    category: 'sofa',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '18',
+    name: 'anastasia',
+    imgUrl: sofa_anastasia,
+    price: 357,
+    category: 'sofa',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '19',
+    name: 'flynn',
+    imgUrl: sofa_flynn,
+    price: 440,
+    category: 'sofa',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '20',
+    name: 'jasmin',
+    imgUrl: sofa_jasmin,
+    price: 458,
+    category: 'sofa',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+  {
+    id: '21',
+    name: 'michau',
+    imgUrl: sofa_michau,
+    price: 543,
+    category: 'sofa',
+    shortDesc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!',
+  },
+];
+
+export default products;
